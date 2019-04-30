76cce0d49c323a1240e88522f0928ed4e4680c7f
Rik Smith-Unna <rik@fathomlabs.io>
2019-04-30 18:40:54 +0100

    tweak sessions for demo

--- server/auth/sessions.js
+++ server/auth/sessions.js
@@ -1,2 +1,8 @@
 var session = require('express-session')
-module.exports = session({ secret: 'prereview-demo', resave: false, saveUninitialized: false })
+module.exports = session({
+  secret: 'prereview-demo',
+  resave: false,
+  rolling: true,
+  saveUninitialized: false,
+  cookie: { path: '/', httpOnly: false, secure: false, maxAge: null }
+})
