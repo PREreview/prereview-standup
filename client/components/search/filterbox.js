d7ef4dcfb478b6e72152f78971f5113605eedb2a
Rik Smith-Unna <rik@fathomlabs.io>
2019-05-02 13:26:44 +0100

    move search out to header

--- client/components/search/filterbox.js
+++ client/components/search/filterbox.js
@@ -1,7 +1,6 @@
 var html = require('choo/html')
 
 var button = require('../button')
-var input = require('../form/typeahead')
 
 module.exports = function (opts) {
   return filterbox
@@ -11,38 +10,21 @@ module.exports = function (opts) {
 
     var btn_date = button(state, emit, {
       label: 'most recent',
-      classes: `ml1 f6 ${filterbydate ? 'bg-light-red' : 'bg-mid-gray'} white`,
+      classes: `ml1 f6 ${filterbydate ? 'bg-light-red' : 'bg-dark-gray'} white`,
       secondary: true
     })
     btn_date.onclick = () => emit('sort', { scope: 'main', sort: 'date' })
 
     var btn_reviews = button(state, emit, {
       label: 'most reviews',
-      classes: `ml2 f6 ${!filterbydate ? 'bg-light-red' : 'bg-mid-gray'} white`,
+      classes: `ml2 f6 ${!filterbydate ? 'bg-light-red' : 'bgdark-gray'} white`,
       secondary: true
     })
     btn_reviews.onclick = () => emit('sort', { scope: 'main', sort: 'reviews' })
 
-    var searchopts = {
-      id: 'main-search-input',
-      entries: Object.values(state.preprints),
-      container: {
-        class: 'bg-white dark-gray'
-      },
-      input: {
-        class: 'bg-white dark-gray b--dark-gray ba pa2',
-        placeholder: 'filter preprints'
-      },
-      onresults: results => emit('preprint-search:results', results)
-    }
-    var search = input(state, emit, searchopts)
-
     return html`
       <div class="flex flex-row tc justify-between items-center">
-        <div class="flex flex-row mh3" style="flex-grow: 1;">
-          ${search}
-        </div>
-        Sort by: ${btn_date} ${btn_reviews}
+        ${btn_date} ${btn_reviews}
       </div>
     `
   }
