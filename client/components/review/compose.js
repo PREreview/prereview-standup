cdfce1f68b0d4a0270fbd91b279dbded793857cf
Richard Smith-Unna <rik@fathomlabs.io>
2019-05-02 18:54:37 +0100

    add context (docs above, actions below) to review composer

--- client/components/review/compose.js
+++ client/components/review/compose.js
@@ -21,10 +21,32 @@ module.exports = function view (state, emit) {
 
   var editorinner = html`<div id="editor" class="flex ${editorstyle}"></div>`
 
+  var btnstyle = state.style.classes.secondaryButton
+
+  var submit = html`
+    <div class="flex flex-row justify-center content-center items-center v-mid bn h2 f5 bg-light-red white link dim outline-0 pa2 pointer">Publish now</div>
+  `
+  submit.onclick = () => emit('pushState', state.href.replace('/new', '/submitted'))
+
   var editorel = html`
   
-  <div class="flex flex-column w-100 pa3">
+  <div class="flex flex-column h-100 w-100 pa3">
+    <div class="flex flex-column lh-copy pa3">
+      <p class="mt0 f4">Thank you for taking the time to PREreview this preprint.</p>
+
+      <p>
+        <b>Before you begin:</b> please make sure you've read our <a class="link dim red" target="_blank" href="/docs/code_of_conduct">code of conduct</a>.
+        You might also find our <a class="link dim red" target="_blank" href="/docs/prereview_guidelines">PREreview guidelines</a> useful, and you can use our templates to help you get started.
+      </p>
+    </div>
     ${editorinner}
+    <div class="flex flex-row justify-between pv2">
+      <div><button class="flex flex-row justify-center content-center items-center v-mid bn h2 f5 bg-gray white link dim outline-0">choose a template</button></div>
+      <div class="flex flex-row">
+        ${submit}
+        <div class="flex flex-row justify-center content-center items-center v-mid bn h2 f5 bg-gray white link dim outline-0 ml2 pa2 pointer">Invite a collaborator</div>
+      </div>
+    </div>
   </div>
   
   `
