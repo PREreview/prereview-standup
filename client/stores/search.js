fb5b30f44623ea64b27cebd2b12008b5a4a093ab
Richard Smith-Unna <rik@fathomlabs.io>
2019-05-02 18:49:45 +0100

    record when first search is conducted

--- client/stores/search.js
+++ client/stores/search.js
@@ -1,10 +1,12 @@
 // var debounce = require('lodash/debounce')
 
 module.exports = function (state, emitter) {
-  state.searchResults = Object.values(state.preprints).slice(0, 20)
+  state.searched = false
+  state.searchResults = Object.values(state.preprints).slice(0, 22)
 
   emitter.on('DOMContentLoaded', async function () {
     emitter.on('preprint-search:results', results => {
+      state.searched = true
       state.searchResults = results
       emitter.emit('render')
     })
