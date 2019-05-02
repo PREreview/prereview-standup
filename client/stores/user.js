18bfabbe1fe43497d63c1e72548b08883655f536
Richard Smith-Unna <rik@fathomlabs.io>
2019-05-02 18:52:01 +0100

    add mocked pictures for users

--- client/stores/user.js
+++ client/stores/user.js
@@ -12,12 +12,14 @@ module.exports = async (state, emitter) => {
   try {
     var userdata = await fetch('/userdata')
     state.user = await userdata.json()
+    var randompic = await fetch('https://randomuser.me/api/?gender=male&inc=picture&noinfo')
+    state.user.picture = (await randompic.json()).results[0].picture
     if (state.contentloaded) {
       emitter.emit('render')
     } else {
       state.renderonload = true
     }
   } catch (e) {
-    console.log('there is no user')
+    console.log('there is no user', e)
   }
 }
