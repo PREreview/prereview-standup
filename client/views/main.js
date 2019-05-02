732ca0a2584c2559b19b01b68c94427b3f981c60
Richard Smith-Unna <rik@fathomlabs.io>
2019-05-02 18:51:11 +0100

    reactive description of results

--- client/views/main.js
+++ client/views/main.js
@@ -13,7 +13,9 @@ function view (state, emit) {
   if (state.title !== TITLE) emit(state.events.DOMTITLECHANGE, TITLE)
 
   var nresults = state.searchResults.length
-  var resultstr = nresults ? `${nresults} results` : 'no results'
+  var resultstr = state.searched ?
+    (nresults ? `${nresults} results` : 'no results') :
+    (`20 most recentely reviewed`)
 
   return html`
     <body class="flex flex-column w-100 justify-center items-center space-around dark-gray">
@@ -22,8 +24,8 @@ function view (state, emit) {
       <div class="w-60 flex flex-row">
         <div class="content fl w-100 pa2 f7 lh-copy">
           <div class="actions flex justify-between pv3">
-            <div class="ttu tracked">
-              ${resultstr}
+            <div class="flex items-center ttu tracked f6">
+              showing: ${resultstr}
             </div>
             ${filterbox(state, emit)}
           </div>
