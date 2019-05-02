4a6d390f63d487fdaf27d0e6c2b0337541881f44
Rik Smith-Unna <rik@fathomlabs.io>
2019-05-02 13:25:34 +0100

    link doi, adjust action wording, adjust layout and styling

--- client/components/cards/preprint.js
+++ client/components/cards/preprint.js
@@ -7,40 +7,49 @@ function preprint (state, emit, p) {
 
   var gotoreviews = () => emit('pushState', `/reviews/${d.doi}`)
 
-  var title = html`<div style="cursor: pointer;">${d.title}</div>`
+  var title = html`<div class="link dim" style="cursor: pointer;">${d.title}</div>`
   title.onclick = gotoreviews
 
   var showreviews = html`
-    <div class="mr2 red" style="cursor: pointer;">${p.reviews} reviews</div>
+    <div class="mr2 dark-gray link dim" style="cursor: pointer;">${p.reviews} reviews</div>
   `
   showreviews.onclick = gotoreviews
 
   var addreview = html`
-    <div class="ml2 red" style="cursor: pointer;">Review this preprint</div>
+    <div class="ml2 red link dim" style="cursor: pointer;">write a prereview</div>
   `
   addreview.onclick = gotoreviews
 
+  var reqreview = html`
+  <div class="ml2 mr2 red link dim" style="cursor: pointer;">request feedback</div>
+`
+  reqreview.onclick = gotoreviews
+
   return html`
   
-  <div class="article w-100 bb b--black-10 pa2 pb3 mb3">
-    <div class="topic w-100 pv1">
-      ${d.tags && d.tags.join(', ')}
+  <div class="flex flex-column article w-100 bb b--black-10 pa2 pb3 mb3 lh-copy">
+    <div class="flex flex-row w-100 justify-between">
+      <div class="topic pv1">
+        ${d.tags && d.tags.join(', ')}
+      </div>
+      <div class="topic pv1">
+        doi:<a class="link dim dark-gray" href="https://doi.org/${d.doi}">${d.doi}</doi>
+      </div>
     </div>
 
     <div class="title w-100" >
-      <h3>${title}</h3>
-      <p class="">
+      <h3 class="fw5 f5 ma0 mt1 lh-title">${title}</h3>
+      <div class="f6 fw3 i mb1">
         ${d.authors.map(a => `${a.firstName} ${a.lastName}`).join(', ')}
-      </p>
+      </div>
     </div>
     
-    <div class="footer flex justify-between pv1">
-      <div class="left i">
+    <div class="footer flex justify-between pt1 pb2">
+      <div class="left f6 fw3">
         Submitted by anon on 18 Jan, 2019
       </div>
       <div class="flex flex-row right"> 
-        ${showreviews} |
-        ${addreview}
+        ${showreviews} | ${reqreview} | ${addreview}
       </div>
     </div>
   </div>
