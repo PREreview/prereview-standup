0bba39fa3c8d71cb27c43dbd1e831a4f057befba
Richard Smith-Unna <rik@fathomlabs.io>
2019-05-02 14:21:06 +0100

    review mocking setup

--- client/stores/devdata.js
+++ client/stores/devdata.js
@@ -3,12 +3,25 @@ module.exports = store
 var preprintdata = require('../assets/data/preprint_seed_data.json')
 var reviewdata = require('../assets/data/review_seed_data.json')
 
+var random = require('lodash/random')
+var fakedoiprefix = random(10000, 99999)
+var fakedoi = () => `10.${fakedoiprefix}/${random(100000, 500000)}`
+
 function store(state, emitter) {
   state.preprints = {}
-  state.reviews = reviewdata
+  state.reviews = {}
 
   preprintdata.forEach(p => {
+    p.reviews = []
     state.preprints[p.doi] = p
+  })
+
+  reviewdata.forEach(r => {
+    var doi = fakedoi()
+
+    r.doi = doi
+    state.reviews[doi] = r
+    state.preprints[r.preprint].reviews.push(doi)
   })
 
   emitter.on('DOMContentLoaded', function () { })
