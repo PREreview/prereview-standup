c17dc70b169eab7f75189047df59dddb26c93181
Rik Smith-Unna <rik@fathomlabs.io>
2019-08-02 16:32:40 +0100

    convert filterbox to dropdown

--- client/components/search/filterbox.js
+++ client/components/search/filterbox.js
@@ -1,4 +1,48 @@
 var html = require('choo/html')
+var css = require('sheetify')
+
+var selectstyle = css`
+
+:host {
+  outline: none;
+  display: block;
+  font-weight: 300;
+  color: black;
+  line-height: 1.3;
+  padding: .6em 1.4em .5em .8em;
+  width: 150px;
+  box-sizing: border-box;
+  margin: 0;
+  border: 0;
+  box-shadow: 0 1px 0 1px rgba(0,0,0,.04);
+  border-radius: .5em;
+  -moz-appearance: none;
+  -webkit-appearance: none;
+  appearance: none;
+  background-color: #d5d4d4;
+}
+
+:host::-ms-expand {
+  display: none;
+}
+
+:host:hover {
+  border-color: #888;
+}
+
+:host:focus {
+  border-color: #aaa;
+  box-shadow: 0 0 1px 3px rgba(59, 153, 252, .7);
+  box-shadow: 0 0 0 3px -moz-mac-focusring;
+  color: white; 
+  outline: none;
+}
+
+:host option {
+  font-weight:normal;
+}
+
+`
 
 var button = require('../button')
 
@@ -8,24 +52,19 @@ module.exports = function (opts) {
   function filterbox (state, emit) {
     var filterbydate = state.filters.main.sort === 'date'
 
-    var btn_date = button(state, emit, {
-      label: 'most recent',
-      classes: `ml1 f6 ${filterbydate ? 'bg-light-red' : 'bg-dark-gray'} white`,
-      secondary: true
-    })
-    btn_date.onclick = () => emit('sort', { scope: 'main', sort: 'date' })
-
-    var btn_reviews = button(state, emit, {
-      label: 'most reviews',
-      classes: `ml2 f6 ${!filterbydate ? 'bg-light-red' : 'bg-dark-gray'} white`,
-      secondary: true
-    })
-    btn_reviews.onclick = () => emit('sort', { scope: 'main', sort: 'reviews' })
-
-    return html`
-      <div class="flex flex-row tc justify-between items-center">
-        ${btn_date} ${btn_reviews}
-      </div>
+    var select = html`
+  
+    <select class="${selectstyle}">
+      <option value="0">Sort by...</option>
+      <option value="1">Most recent</option>
+      <option value="2">Most popular</option>
+    </select>
+    
     `
+
+    // btn_date.onclick = () => emit('sort', { scope: 'main', sort: 'date' })
+    // btn_reviews.onclick = () => emit('sort', { scope: 'main', sort: 'reviews' })
+
+    return select
   }
 }
