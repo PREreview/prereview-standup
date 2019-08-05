0c4ad522d7ac537ed817cbb7550e54b668158765
Rik Smith-Unna <rik@fathomlabs.io>
2019-08-05 13:18:13 +0100

    server

--- server/routes/data/users/me.js
+++ server/routes/data/users/me.js
@@ -6,7 +6,7 @@ var router = express.Router()
 
 // Serves the current user's own data to the client app
 // only works if the user is logged in - otherwise req.user is null
-router.get('/', function (req, res) {
+router.get('/me', function (req, res) {
 	if (req.user) {
 		res.json(req.user)
 	} else {
@@ -17,7 +17,7 @@ router.get('/', function (req, res) {
 
 // Allows the current user to switch their account to private
 // so that their real name and ORCID are hidden
-router.post('/become_private', function (req, res) {
+router.post('/me/become_private', function (req, res) {
 	if (req.user) {
 		user.makeUserPrivate(req.user).then(res.json(req.user))
 	} else {
@@ -28,7 +28,7 @@ router.post('/become_private', function (req, res) {
 
 // Allows the current user to switch their account to public
 // so that their real name and ORCID are shown
-router.post('/become_public', function (req, res) {
+router.post('/me/become_public', function (req, res) {
 	if (req.user) {
 		user.makeUserPublic(req.user).then(res.json(req.user))
 	} else {
