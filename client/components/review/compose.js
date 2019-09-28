68aeec376739a576bd8dc6501d75652d075ec2d5
Rik Smith-Unna <rik@fathomlabs.io>
2019-09-28 20:30:44 +0100

    If PDF is blocked don't show metadata in review pane

--- client/components/review/compose.js
+++ client/components/review/compose.js
@@ -30,9 +30,19 @@ css`
 module.exports = function view (state, emit, preprint) {
   var editor = state.cache(Editor, `editor-${preprint.id.replace('/', '-')}`)
 
-  var publisher = html`<div class="red i b">${preprint.publisher}</div>`
-  var title = html`<h1 class="mv1 lh-solid">${preprint.title}</h1>`
-  var authors = html`<h2 class="f4 mv1 i lh-title">${preprint.authors.list.list.map(a => a.fullName).join(', ')}</h2>`
+  var maybemeta = () => {
+    if (preprint.pdfblocked) return null
+    var publisher = html`<div class="red i b">${preprint.publisher}</div>`
+    var title = html`<h1 class="mv1 lh-solid">${preprint.title}</h1>`
+    var authors = html`<h2 class="f4 mv1 i lh-title">${preprint.authors.list.list.map(a => a.fullName).join(', ')}</h2>`
+    return html`
+    <div class="flex flex-column w-100">
+      ${publisher}
+      <a class="black link" href="https://doi.org/${preprint.id.replace('doi/', '')}" target="_blank">${title}</a>
+      ${authors}
+    </div>
+    `
+  }
 
   var submit = html`
     <div class="${btnclasses} bg-red">Publish now</div>
@@ -44,10 +54,8 @@ module.exports = function view (state, emit, preprint) {
   
   <div class="flex flex-column h-100 w-100 ph2 pv0">
     <div class="flex flex-column lh-copy pa3">
-      ${publisher}
-      <a class="black link" href="https://doi.org/${preprint.id.replace('doi/', '')}" target="_blank">${title}</a>
-      ${authors}
-      <p>
+      ${maybemeta()}
+      <p class="mv1">
         <b>Before you begin:</b> please make sure you've read our <a class="link dim red" target="_blank" href="/docs/code_of_conduct">code of conduct</a>.
         You might also find our <a class="link dim red" target="_blank" href="/docs/resources">PREreview guidelines</a> useful.
       </p>
