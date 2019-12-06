4099fffd5f0a849aade430e160be91f473616214
Alexandru Patrascu <alexandru.patrascu@wingravity.com>
2019-12-06 17:18:05 +0200

    migration

--- server/db/tables/reviewrequests/schema.js
+++ server/db/tables/reviewrequests/schema.js
@@ -6,7 +6,7 @@ function createTable () {
   return db.schema.dropTableIfExists('reviewrequests').then(function () {
     return db.schema.createTable('reviewrequests', table => {
       table.increments('id').primary()
-      table.string('prereview_id').notNullable().references('prereviews.prereview_id').index()
+      table.string('preprint_id').notNullable().references('preprints.id').index()
       table.integer('author_id').notNullable().references('users.user_id').index()
       table.timestamp('date_created').defaultTo(db.fn.now())
     })
