7248abe51c8c5e27da4a487110104476373db17e
Gabriel <gabrielcazacu96@gmail.com>
2020-01-13 10:52:41 +0200

    [PRE-44]

--- client/components/search/filterbox.js
+++ client/components/search/filterbox.js
@@ -49,14 +49,14 @@ module.exports = function (opts) {
 
 function filterBtn (state, emit) {
   var sortBtn = html`
-    <button class="${selectstyle} pointer">
+    <button style="display: block; font-size: 15px; height: 32px;" class="${selectstyle} pointer">
       ${state.sort.desc}
-      <img src="data:image/svg+xml;charset=US-ASCII,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%22292.4%22%20height%3D%22292.4%22%3E%3Cpath%20fill%3D%22%23333333%22%20d%3D%22M287%2069.4a17.6%2017.6%200%200%200-13-5.4H18.4c-5%200-9.3%201.8-12.9%205.4A17.6%2017.6%200%200%200%200%2082.2c0%205%201.8%209.3%205.4%2012.9l128%20127.9c3.6%203.6%207.8%205.4%2012.8%205.4s9.2-1.8%2012.8-5.4L287%2095c3.5-3.5%205.4-7.8%205.4-12.8%200-5-1.9-9.2-5.5-12.8z%22%2F%3E%3C%2Fsvg%3E"/>
+      <img style="width: 10px; margin-left: 5px;" src="data:image/svg+xml;charset=US-ASCII,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%22292.4%22%20height%3D%22292.4%22%3E%3Cpath%20fill%3D%22%23333333%22%20d%3D%22M287%2069.4a17.6%2017.6%200%200%200-13-5.4H18.4c-5%200-9.3%201.8-12.9%205.4A17.6%2017.6%200%200%200%200%2082.2c0%205%201.8%209.3%205.4%2012.9l128%20127.9c3.6%203.6%207.8%205.4%2012.8%205.4s9.2-1.8%2012.8-5.4L287%2095c3.5-3.5%205.4-7.8%205.4-12.8%200-5-1.9-9.2-5.5-12.8z%22%2F%3E%3C%2Fsvg%3E"/>
     </button>
   `
 
   var sortDropdown = html`
-    <div id="search-sort-dd" class="bg-white flex-column dark-gray nowrap ${ddstyle} dn">
+    <div id="search-sort-dd" style="position: absolute; top: 35px; left: 0; width: auto;" class="bg-white flex-column dark-gray nowrap ${ddstyle} dn">
     </div>
   `
 
